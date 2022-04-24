f02d9ed834a819ee7b1fae21019ebd4fe9189d8d
rydvone <rydvone@gmail.com>
2022-04-24 10:14:17 +0300

    feat: start work w slider

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -5,11 +5,66 @@ import dataJSON from './../../assets/src/components/data.js';
 // wait why all pages html + css will be load
 window.onload = () => {
   const data = dataJSON;
+  // components
+  burgerSubscribe();
+  popupSubscribe();
+
+  console.log('Hello, friend!')
+
+
+
+  /* -------------------carousel------------------ */ 
+  const sliderBtnLeft = document.querySelector('.slide__button_prev');
+  const sliderBtnRight = document.querySelector('.slide__button_next');
+  const slides = document.querySelector('.slides');
+
+  function moveLeft() {
+    slides.classList.add('move-to-left');
+    // disable for the duration of the animation
+    sliderBtnLeft.removeEventListener('click', moveLeft);
+  }
+  function moveRight() {
+    slides.classList.add('move-to-right');
+    // disable for the duration of the animation
+    sliderBtnRight.removeEventListener('click', moveRight);
+  }
+
+  sliderBtnLeft.addEventListener('click', moveLeft);
+  sliderBtnRight.addEventListener('click', moveRight);
+  slides.addEventListener('animationend', (ev) => {
+    // console.log(ev);
+    if (ev.animationName === 'move-left') {
+      console.log('some left');
+      slides.classList.remove('move-to-left');
+      const slideLeft = document.querySelector('#slide-left');
+      console.log(slideLeft);
+    }
+    if (ev.animationName === 'move-right') {
+      console.log('some tooooo right');
+      slides.classList.remove('move-to-right');
+    }   
+
+    sliderBtnLeft.addEventListener('click', moveLeft);
+    sliderBtnRight.addEventListener('click', moveRight);
+  })
 
 
 
 
-// stackoverflow code
+
+
+
+
+
+
+
+
+
+
+
+
+
+// stackoverflow code to disable scroll
 const { body, documentElement } = document;
 let { scrollTop } = document.documentElement;
 
@@ -24,16 +79,22 @@ function enableScroll() {
   documentElement.scrollTop = scrollTop;
   body.style.removeProperty("top");
 }
+// If you are using scroll-behavior: smooth on html, you also need to modify the enableScroll function like this:
+
+function enableScroll() {
+  body.classList.remove("scroll-disabled");
+  // Set "scroll-behavior" to "auto"
+  documentElement.style.scrollBehavior = "auto";
+  documentElement.scrollTop = scrollTop;
+  // Remove "scroll-behavior: auto" after restoring scroll position
+  documentElement.style.removeProperty("scroll-behavior");
+  body.style.removeProperty("top");
+}
 
 
 
 
 
 
-  // components
-  burgerSubscribe();
-  popupSubscribe();
-
-  console.log('Hello, friend!')
 
 }
