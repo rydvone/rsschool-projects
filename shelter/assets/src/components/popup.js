e809033b42dc42fd0a837bf9bfa699108973d782
rydvone <rydvone@gmail.com>
2022-04-24 23:16:26 +0300

    refactor: work w popup

--- shelter/assets/src/components/popup.js
+++ shelter/assets/src/components/popup.js
@@ -1,36 +1,35 @@
 'use strict'
-const slide = [...document.querySelectorAll('.slide')];
+// const slide = [...document.querySelectorAll('.slide')];
+// const slide = document.querySelectorAll('.slide');
 const buttonClose = document.querySelector('.popup__button-close');
 const body = document.querySelector('.body');
 const popupBlock = document.querySelector('.popup-block');
 const popupCard = document.querySelector('.popup__card'); 
+const imgAbout = document.querySelector('.img__about'); 
 
 
 const popupSwitch = () => {
   console.log('hellooooooooo');
   popupBlock.classList.toggle('open');
   body.classList.toggle('active');
-
-  // if (popupBlock.contains('open')) {
-  //   disableScroll();
-  // }
-
 }
-// e.target == popupBlock || !(popupCard.contains(e.target))
+
+
 const outsidePopup = (e) => {
   if(!(popupCard.contains(e.target))) {
     console.log('block shadow');
     popupSwitch();
    }
 }
-
+const slide = [...document.querySelectorAll('.slide')];
 const popupSubscribe = () => {
-  buttonClose.addEventListener('click', popupSwitch);
-  slide.forEach((el) => el.addEventListener('click', popupSwitch));
+  slide.forEach((el) => el.addEventListener('click', (e) => {
+    console.log(e.currentTarget);
+  }));
 
+  // buttonClose.addEventListener('click', popupSwitch);
   // popupCard.addEventListener('click', popupSwitch);
   // document.addEventListener('click', outsidePopup);
-
 }
 
 
--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -1,5 +1,5 @@
 import burgerSubscribe from "./../../assets/src/components/burger.js";
-import popupSubscribe from "./../../assets/src/components/popup.js";
+// import popupSubscribe from "./../../assets/src/components/popup.js";
 import dataJSONfile from "./../../assets/src/components/data.js";
 import sliderSubscribe from "./../../assets/src/components/slider.js";
 
@@ -9,14 +9,54 @@ window.onload = () => {
   const dataJSON = dataJSONfile;
   // components
   burgerSubscribe();
-  popupSubscribe();
+  // popupSubscribe();
   sliderSubscribe(dataJSON);
 
 
   console.log("Hello, friend!");
 
+  // const slide = [...document.querySelectorAll('.slide')];
+  // slide.forEach((el) => el.addEventListener('click', (e) => {
+  //   console.log(e.currentTarget);
+  // }));
 
-
+  const slide = [...document.querySelectorAll('.slide')];
+  const buttonClose = document.querySelector('.popup__button-close');
+  const body = document.querySelector('.body');
+  const popupBlock = document.querySelector('.popup-block');
+  const popupCard = document.querySelector('.popup__card'); 
+  const imgAbout = document.querySelector('.img__about'); 
+  let namePets = '';
+  
+  const popupSwitch = (e) => {
+    popupBlock.classList.toggle('open');
+    body.classList.toggle('active');
+    // console.log(e.currentTarget.textContent);
+    // let lengthNamePets = e.currentTarget.textContent.length;
+    // namePets = e.currentTarget.textContent.slice(0, (lengthNamePets - 10));
+    // console.log(namePets);
+  }
+  
+  
+  const outsidePopup = (e) => {
+    if(!(popupCard.contains(e.target))) {
+      console.log('block shadow');
+      popupSwitch();
+     }
+  }
+  
+    buttonClose.addEventListener('click', popupSwitch);
+    slide.forEach((el) => el.addEventListener('click', popupSwitch));
+  
+    document.addEventListener('click', (e) => {
+      if (e.target.closest('.slide') != null) {
+        popupSwitch(e);
+      }
+    })
+    
+    // popupCard.addEventListener('click', popupSwitch);
+    // document.addEventListener('click', outsidePopup);
+    // e.target.closest('.popup')
 
   
 
@@ -24,29 +64,29 @@ window.onload = () => {
 
 
   // stackoverflow code to disable scroll
-  const { body, documentElement } = document;
-  let { scrollTop } = document.documentElement;
+  // const { body, documentElement } = document;
+  // let { scrollTop } = document.documentElement;
 
-  function disableScroll() {
-    scrollTop = documentElement.scrollTop;
-    body.style.top = `-${scrollTop}px`;
-    body.classList.add("scroll-disabled");
-  }
+  // function disableScroll() {
+  //   scrollTop = documentElement.scrollTop;
+  //   body.style.top = `-${scrollTop}px`;
+  //   body.classList.add("scroll-disabled");
+  // }
 
-  function enableScroll() {
-    body.classList.remove("scroll-disabled");
-    documentElement.scrollTop = scrollTop;
-    body.style.removeProperty("top");
-  }
-  // If you are using scroll-behavior: smooth on html, you also need to modify the enableScroll function like this:
+  // function enableScroll() {
+  //   body.classList.remove("scroll-disabled");
+  //   documentElement.scrollTop = scrollTop;
+  //   body.style.removeProperty("top");
+  // }
+  // // If you are using scroll-behavior: smooth on html, you also need to modify the enableScroll function like this:
 
-  function enableScroll() {
-    body.classList.remove("scroll-disabled");
-    // Set "scroll-behavior" to "auto"
-    documentElement.style.scrollBehavior = "auto";
-    documentElement.scrollTop = scrollTop;
-    // Remove "scroll-behavior: auto" after restoring scroll position
-    documentElement.style.removeProperty("scroll-behavior");
-    body.style.removeProperty("top");
-  }
+  // function enableScroll() {
+  //   body.classList.remove("scroll-disabled");
+  //   // Set "scroll-behavior" to "auto"
+  //   documentElement.style.scrollBehavior = "auto";
+  //   documentElement.scrollTop = scrollTop;
+  //   // Remove "scroll-behavior: auto" after restoring scroll position
+  //   documentElement.style.removeProperty("scroll-behavior");
+  //   body.style.removeProperty("top");
+  // }
 };
