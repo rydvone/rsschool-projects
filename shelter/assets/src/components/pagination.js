ab2d4f3efbae6a9f4e65caa07686ae9015004ecb
rydvone <rydvone@gmail.com>
2022-04-25 20:12:23 +0300

    feat: add pagination only 8

--- shelter/assets/src/components/pagination.js
+++ shelter/assets/src/components/pagination.js
@@ -1,8 +1,7 @@
 "use strict";
 
-/* -------------------carousel------------------ */
-
 function paginationSubscribe(dataJSON) {
+  const slider = document.querySelector(".slider");
   const buttonDoubleLess = document.querySelector(".double-less");
   const buttonLess = document.querySelector(".less");
   const buttonDoubleMore = document.querySelector(".double-more");
@@ -11,12 +10,12 @@ function paginationSubscribe(dataJSON) {
 
 
 
-  const sliderBtnLeft = document.querySelector(".slide__button_prev");
-  const sliderBtnRight = document.querySelector(".slide__button_next");
-  const slides = document.querySelector(".slides");
-  const wrapperSlideLeft = document.querySelector("#slide-left");
-  const wrapperSlideRight = document.querySelector("#slide-right");
-  const wrapperSlideActive = document.querySelector("#slide-active");
+  // const sliderBtnLeft = document.querySelector(".slide__button_prev");
+  // const sliderBtnRight = document.querySelector(".slide__button_next");
+  // const slides = document.querySelector(".slides");
+  // const wrapperSlideLeft = document.querySelector("#slide-left");
+  // const wrapperSlideRight = document.querySelector("#slide-right");
+  // const wrapperSlideActive = document.querySelector("#slide-active");
   const windowInnerWidth = window.innerWidth;
   let numCurrentPage = 1;
   let numMaxPage = 6;
@@ -39,19 +38,53 @@ function paginationSubscribe(dataJSON) {
 
 
   function loadPage() {
-    buttonDoubleLess.disabled = true;
-    buttonDoubleLess.classList.add('disable');
-    buttonLess.disabled = true;
-    buttonLess.classList.add('disable');
+    page.textContent = numCurrentPage;
+    numCurrentPageIsStart();
+    if (numSlide == 8) {
+      arrPetsPage8 = [...arrPetsSlideAll];
+    }
   }
 
 
   // create common array for all pages
-  function createArrayPetsAll() {
+  function createArrayPetsCommon() {
     for (let i = 0; i < 6; i++) {
       arrPetsSlideAll.push(createRandomPage(8));
     }
   }
+
+  function createArrayPetsForAllPage() {
+    let countSlide = 6;
+    let countMaxPage = 8;
+    let arrRandomInner = [];
+    // let arrPetsPageTemp = arrPetsSlideAll.join().split();
+    // let min = 0;
+    // let max = arrPetsPageTemp.length;
+    // let rndNum = 0;
+
+    // const random = (min, max) => {
+    //   return Math.floor(Math.random() * (max - min + 1)) + min;
+    // };
+
+    // for (let i = 0; i < countMaxPage; i++) {      
+    //   let arrRandomInnerTemp = [];
+    //   rndNum = random(min, max);
+    //   arrRandomInnerTemp.push(arrPetsPageTemp[rndNum]);
+    //   arrPetsPageTemp.splice(rndNum, 1);
+    //   max--;
+    //   for (let j = 0; j < countSlide - 1; j++) {
+    //     rndNum = random(min, max);
+    //     while (arrRandomInnerTemp.indexOf(arrPetsPageTemp[rndNum]) !== -1) {
+    //       rndNum = random(min, max);
+    //     }
+    //     arrRandomInnerTemp.push(arrPetsPageTemp[rndNum]);
+    //     arrPetsPageTemp.splice(rndNum, 1);
+    //     max--;
+    //   }
+    //   arrRandomInner.push(arrRandomInnerTemp);
+    // }
+  }
+  
 
   // random
   function createRandomPage(numPetsCard) {
@@ -66,29 +99,22 @@ function paginationSubscribe(dataJSON) {
       }
       arrRandomInner.push(rndNum);
     }
-    console.log("arrRandomInner = " + arrRandomInner);
+    // console.log("arrRandomInner = " + arrRandomInner);
     return arrRandomInner;
   }
   //   arrNumPetsActive = [...arrRandomInner];
 
 
-
-  createArrayPetsAll();
+  // createRandomPage(numSlide);
+  createArrayPetsCommon();
   console.log(arrPetsSlideAll);
-  createRandomPage(numSlide);
   loadPage();
+  createArrayPetsForAllPage();
+  createSlide(slider, numCurrentPage);
 
-  // after onload bumber page = 
-  page.textContent = numCurrentPage;
+  
 
 
-  if (numSlide == 8) {
-    arrPetsPage8 = [...arrPetsSlideAll];
-  }
-  if (numSlide == 6) {
-    page.textContent = numCurrentPage;
-
-  }
 
   function numCurrentPageIsStart() {
     buttonDoubleLess.disabled = true;
@@ -147,13 +173,14 @@ function paginationSubscribe(dataJSON) {
 
   function moveDoubleLess() {
     numCurrentPage = 1;
+    createSlide(slider, numCurrentPage);
     numCurrentPageIsStart();
-
     page.textContent = numCurrentPage;
   }
 
   function moveLess() {
     numCurrentPage--;
+    createSlide(slider, numCurrentPage);
     if (numCurrentPage != numMaxPage) {
       numCurrentPageIsMiddle();
     }
@@ -165,20 +192,20 @@ function paginationSubscribe(dataJSON) {
 
   function moveMore() {
     numCurrentPage++;
+    createSlide(slider, numCurrentPage);
     if (numCurrentPage != 1) {
       numCurrentPageIsMiddle();
     }
     if (numCurrentPage == numMaxPage) {
       numCurrentPageIsEnd();
     }
-
     page.textContent = numCurrentPage;
   }
 
   function moveDoubleMore() {
     numCurrentPage = numMaxPage;
+    createSlide(slider, numCurrentPage);
     numCurrentPageIsEnd();
-
     page.textContent = numCurrentPage;
   }
 
@@ -189,22 +216,12 @@ function paginationSubscribe(dataJSON) {
 
 
 
+
+
+
   //   let arrPetsPage6Temp = arrPetsSlideAll.join().split();
 
 
-
-  // function moveLeft() {
-  //   slides.classList.add("move-to-left");
-  //   // disable for the duration of the animation
-  //   sliderBtnLeft.removeEventListener("click", moveLeft);
-  //   sliderBtnRight.removeEventListener("click", moveRight);
-  // }
-  // function moveRight() {
-  //   slides.classList.add("move-to-right");
-  //   // disable btn for the duration of the animation
-  //   sliderBtnLeft.removeEventListener("click", moveLeft);
-  //   sliderBtnRight.removeEventListener("click", moveRight);
-  // }
 
   // createSlide(wrapperSlideActive, true);
   // createSlide(wrapperSlideLeft, false);
@@ -254,36 +271,36 @@ function paginationSubscribe(dataJSON) {
   // });
 
 
-  // function createSlide(nameSlide) {
-  //   nameSlide.innerHTML = "";
-  //   // let arrRandom = findRandom(ckeckEmptyArr);
+  function createSlide(nameSlide, numPageArg) {
+    nameSlide.innerHTML = "";
+    // let arrRandom = findRandom(ckeckEmptyArr);
 
-  //   for (let i = 0; i < numSlide; i++) {
-  //     // let namePets = dataJSON[arrRandom[i]].name;
+    for (let i = 0; i < numSlide; i++) {
+      let namePets = dataJSON[arrPetsPage8[numPageArg - 1][i]].name;
 
-  //     const tagDivSlide1 = document.createElement("div");
-  //     tagDivSlide1.classList.add("slide");
+      const tagDivSlide1 = document.createElement("div");
+      tagDivSlide1.classList.add("slide");
 
-  //     const tagImgSlide1 = document.createElement("img");
-  //     tagImgSlide1.classList.add("img__slide");
-  //     tagImgSlide1.src = "./../../assets/images/pets/" + namePets.toLowerCase() + ".png";
-  //     tagImgSlide1.alt = "your best friends";
+      const tagImgSlide1 = document.createElement("img");
+      tagImgSlide1.classList.add("img__slide");
+      tagImgSlide1.src = "./../../assets/images/pets/" + namePets.toLowerCase() + ".png";
+      tagImgSlide1.alt = "your best friends";
 
-  //     const tagDivSubtitleSlide1 = document.createElement("div");
-  //     tagDivSubtitleSlide1.classList.add("slide__subtitle");
-  //     tagDivSubtitleSlide1.textContent = namePets;
+      const tagDivSubtitleSlide1 = document.createElement("div");
+      tagDivSubtitleSlide1.classList.add("slide__subtitle");
+      tagDivSubtitleSlide1.textContent = namePets;
 
-  //     const tagButtonSlide1 = document.createElement("button");
-  //     tagButtonSlide1.classList.add("button_transparent");
-  //     tagButtonSlide1.classList.add("slide__button");
-  //     tagButtonSlide1.textContent = "Learn more";
+      const tagButtonSlide1 = document.createElement("button");
+      tagButtonSlide1.classList.add("button_transparent");
+      tagButtonSlide1.classList.add("slide__button");
+      tagButtonSlide1.textContent = "Learn more";
 
-  //     nameSlide.append(tagDivSlide1);
-  //     tagDivSlide1.append(tagImgSlide1);
-  //     tagDivSlide1.append(tagDivSubtitleSlide1);
-  //     tagDivSlide1.append(tagButtonSlide1);
-  //   }
-  // }
+      nameSlide.append(tagDivSlide1);
+      tagDivSlide1.append(tagImgSlide1);
+      tagDivSlide1.append(tagDivSubtitleSlide1);
+      tagDivSlide1.append(tagButtonSlide1);
+    }
+  }
 }
 
 export default paginationSubscribe;
